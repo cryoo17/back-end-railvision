88fb203be08f84e089f56968b193b81747ee1f08
cryoo17 <tgun8113@gmail.com>
2025-05-04 13:42:19 +0700

    feat: init project & auth management

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,214 @@
+import { Request, Response } from "express";
+import * as Yup from "yup";
+import UserModel from "../models/user.model";
+import { encrypt } from "../utils/encryption";
+import { generateToken } from "../utils/jwt";
+import { IReqUser } from "../middlewares/auth.middleware";
+
+type TRegister = {
+  fullName: string;
+  username: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+
+type TLogin = {
+  identifier: string;
+  password: string;
+};
+
+const registerValidateSchema = Yup.object({
+  fullName: Yup.string().required(),
+  username: Yup.string().required(),
+  email: Yup.string().email().required(),
+  password: Yup.string()
+    .required()
+    .min(6, "Password minimal 6 karakter")
+    .test(
+      "at-least-one-uppercase-letter",
+      "Minimal terdapat 1 huruf kapital",
+      (value) => {
+        if (!value) return false;
+        const regex = /^(?=.*[A-Z])/;
+        return regex.test(value);
+      }
+    )
+    .test(
+      "at-least-one-number",
+      "Minimal terdapat 1 angka",
+      (value) => {
+        if (!value) return false;
+        const regex = /^(?=.*\d)/;
+        return regex.test(value);
+      }
+    ),
+  confirmPassword: Yup.string()
+    .required()
+    .oneOf([Yup.ref("password"), ""], "Password not match"),
+});
+
+export default {
+  async register(req: Request, res: Response) {
+    /**
+     #swagger.tags = ['Auth']
+     #swagger.requestBody = {
+      required: true,
+      schema: {$ref: '#/components/schemas/RegisterRequest'}
+     }
+     */
+    const { fullName, username, email, password, confirmPassword } =
+      req.body as unknown as TRegister;
+
+    try {
+      await registerValidateSchema.validate({
+        fullName,
+        username,
+        email,
+        password,
+        confirmPassword,
+      });
+
+      const result = await UserModel.create({
+        fullName,
+        username,
+        email,
+        password,
+      });
+
+      res.status(200).json({
+        message: "Pendaftaran berhasil!",
+        data: result,
+      });
+    } catch (error) {
+      const err = error as unknown as Error;
+
+      res.status(400).json({
+        message: err.message,
+        data: null,
+      });
+    }
+  },
+
+  async login(req: Request, res: Response) {
+    /**
+     #swagger.tags = ['Auth']
+     #swagger.requestBody = {
+      required: true,
+      schema: {$ref: "#/components/schemas/LoginRequest"}
+     }
+     */
+    const { identifier, password } = req.body as unknown as TLogin;
+
+    try {
+      const userByIdentifier = await UserModel.findOne({
+        $or: [
+          {
+            email: identifier,
+          },
+          {
+            username: identifier,
+          },
+        ],
+        isActive: true,
+      });
+
+      if (!userByIdentifier) {
+        return res.status(403).json({
+          message: "User tidak ditemukan",
+          data: null,
+        });
+      }
+
+      const validatePassword: boolean =
+        encrypt(password) === userByIdentifier.password;
+
+      if (!validatePassword) {
+        return res.status(403).json({
+          message: "User tidak ditemukan",
+          data: null,
+        });
+      }
+
+      const token = generateToken({
+        id: userByIdentifier._id,
+        role: userByIdentifier.role,
+      });
+
+      res.status(200).json({
+        message: "Login berhasil",
+        data: token,
+      });
+    } catch (error) {
+      const err = error as unknown as Error;
+
+      res.status(400).json({
+        message: err.message,
+        data: null,
+      });
+    }
+  },
+
+  async me(req: IReqUser, res: Response) {
+    /**
+     #swagger.tags = ['Auth']
+     #swagger.security = [{
+      "bearerAuth": []
+     }]
+     */
+
+    try {
+      const user = req.user;
+      const result = await UserModel.findById(user?.id);
+
+      res.status(200).json({
+        message: "Berhasil mendapatkan profile user",
+        data: result,
+      });
+    } catch (error) {
+      const err = error as unknown as Error;
+
+      res.status(400).json({
+        message: err.message,
+        data: null,
+      });
+    }
+  },
+
+  async activation(req: Request, res: Response) {
+    /**
+     #swagger.tags = ['Auth']
+     #swagger.requestBody = {
+      required: true,
+      schema: {$ref: '#/components/schemas/ActivationRequest'}
+     }
+     */
+
+    try {
+      const { code } = req.body as { code: string };
+      const user = await UserModel.findOneAndUpdate(
+        {
+          activationCode: code,
+        },
+        {
+          isActive: true,
+        },
+        {
+          new: true,
+        }
+      );
+
+      res.status(200).json({
+        message: "Berhasil aktivasi user",
+        data: user,
+      });
+    } catch (error) {
+      const err = error as unknown as Error;
+
+      res.status(400).json({
+        message: err.message,
+        data: null,
+      });
+    }
+  },
+};
---
+++ src/docs/swagger.ts
@@ -0,0 +1,48 @@
+import swaggerAutogen from "swagger-autogen";
+
+const doc = {
+  info: {
+    version: "v0.0.1",
+    title: "Dokumentasi API Acara",
+    description: "Dokumentasi API Acara",
+  },
+  servers: [
+    {
+      url: "http://localhost:3000/api",
+      description: "Local Server",
+    },
+    {
+      url: "https://back-end-acara-xi-three.vercel.app/api",
+      description: "Deploy Server",
+    },
+  ],
+  components: {
+    securitySchemes: {
+      bearerAuth: {
+        type: "http",
+        scheme: "bearer",
+      },
+    },
+    schemas: {
+      LoginRequest: {
+        identifier: "azril",
+        password: "Azril123",
+      },
+      RegisterRequest: {
+        fullName: "Azril Januar",
+        username: "azril",
+        email: "azriljanuar@student.ub.ac.id",
+        password: "Azril123",
+        confirmPassword: "Azril123",
+      },
+      ActivationRequest: {
+        code: "abcdef",
+      },
+    },
+  },
+};
+
+const outputFile = "./swagger_output.json";
+const endpointsFile = ["../routes/api.ts"];
+
+swaggerAutogen({ openapi: "3.0.0" })(outputFile, endpointsFile, doc);
